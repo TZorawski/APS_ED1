e96409f49e4c207e6ad486711f911d96d8251f10
THAIS ZORAWSKI <a1511556@debian9-dacom.dacom>
2017-11-28 18:48:42 -0200

    Base inicial

---
+++ bd.h
@@ -0,0 +1,32 @@
+typedef struct noDado{
+    void* dado;
+    int tamDado;
+    struct noDado* dir;
+    struct noDado* esq;
+    struct noDado* cima;
+    struct noDado* baixo;
+} NoDado;
+
+typedef struct noAtributo{
+    char nome[30];
+    char tipo[30];
+    NoDado* valores;
+    struct noAtributo* dir;
+    struct noAtributo* esq;
+} NoAtributo;
+
+typedef struct {
+    char nome[30];
+    NoAtributo* atributos;
+    NoDado** tuplas;
+    unsigned short int tamTuplas;
+    unsigned short int qtdeTuplas;
+} Tabela;
+
+
+typedef struct {
+    char nome[30];
+    Tabela** tabelas;
+    unsigned short int tamTabelas;
+    unsigned short int qtdeTabelas;
+} Database;
